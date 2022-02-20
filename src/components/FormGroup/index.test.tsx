3e096496e9e9498ec2d1884ecfcbd81fbb63255f
Diego Kfuri <kkfuri@gmail.com>
2022-02-20 14:41:57 -0300

    test: add tests for form group component

---
+++ src/components/FormGroup/index.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+
+import FormGroup from "."
+
+describe("<FormGroup />", () => {
+  it("should render the children and pass props correctly", () => {
+    render(
+      <FormGroup id="formgroup-id" label="label">
+        <input />
+      </FormGroup>
+    )
+    expect(screen.getByLabelText("label")).toHaveAttribute('id', "formgroup-id")
+  })
+
+  it("should render the error message and pass props correctly to input", () => {
+    render(
+      <FormGroup id="formgroup-id" label="label" error="error message example">
+        <input />
+      </FormGroup>
+    )
+
+    const input = screen.getByLabelText("label")
+    expect(input).toHaveAttribute("aria-invalid", "true")
+    expect(input).toHaveAttribute("aria-errormessage", "formgroup-id-error")
+    expect(input).toHaveErrorMessage("Error: error message example")
+  })
+})
